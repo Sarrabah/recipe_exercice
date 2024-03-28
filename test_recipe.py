f2e6e5600bdea840fcabe54e18b3056585a74c0b
sarrabah <sarrabenarbia1996@gmail.com>
2024-03-28 17:50:15 +0100

    feat: Add a failing unit test for the case that the expected numer of person is equal to the desired number of person

--- test_recipe.py
+++ test_recipe.py
@@ -3,3 +3,6 @@ from recipe import print_desirable_recipe
 
 def test_void_actual_recipe():
     assert print_desirable_recipe({}, 2,5) == {}
+
+def test_equal_expected_and_desired_nb_per():
+    assert print_desirable_recipe({"farine": 230, "sucre": 290},2,2) == {"farine": 230, "sucre": 290} 
