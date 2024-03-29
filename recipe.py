7200749c047600add3cd86cd32c41924bcea1045
sarrabah <sarrabenarbia1996@gmail.com>
2024-03-29 17:08:15 +0100

    refactor:  Write the two conditions into a single expression using a logical operator

--- recipe.py
+++ recipe.py
@@ -1,5 +1,3 @@
 def print_desirable_recipe(actual_recipe, expected_nb_per, desired_nb_per):
-    if actual_recipe == {}:
-        return actual_recipe
-    elif expected_nb_per == desired_nb_per:
+    if actual_recipe == {} or expected_nb_per == desired_nb_per :
         return actual_recipe
