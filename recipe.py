0ed9f989fca5fc3193d4b2c7151615032fd0f72f
sarrabah <sarrabenarbia1996@gmail.com>
2024-03-29 17:47:34 +0100

    feat: Handle cases where the expected and desired number of persons differ

--- recipe.py
+++ recipe.py
@@ -1,3 +1,8 @@
 def print_desirable_recipe(actual_recipe, expected_nb_per, desired_nb_per):
     if actual_recipe == {} or expected_nb_per == desired_nb_per :
         return actual_recipe
+    else:
+        final_recipe={}
+        for key in actual_recipe.keys():
+            final_recipe[key] = (actual_recipe[key]/expected_nb_per)*desired_nb_per 
+        return final_recipe
