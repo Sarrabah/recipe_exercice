75cdc76d4bfbee3ecf58d6cdd711a5a4c8820b4b
sarrabah <sarrabenarbia1996@gmail.com>
2024-03-29 16:51:53 +0100

    feat: Write successful code to evaluate the equality between the desired and the expected number

--- recipe.py
+++ recipe.py
@@ -1,3 +1,5 @@
 def print_desirable_recipe(actual_recipe, expected_nb_per, desired_nb_per):
     if actual_recipe == {}:
         return actual_recipe
+    elif expected_nb_per == desired_nb_per:
+        return actual_recipe
