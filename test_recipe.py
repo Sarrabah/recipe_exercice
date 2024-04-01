8bd0b19486af8349c9b70068267fa231e1f82c60
sarrabah <sarrabenarbia1996@gmail.com>
2024-04-01 16:21:14 +0200

    feat: Add a succeded test when the desired number of persons is equal to zero

--- test_recipe.py
+++ test_recipe.py
@@ -9,3 +9,6 @@ def test_equal_expected_and_desired_nb_per():
 
 def test_different_expected_and_desired_nb_per() :
     assert print_desirable_recipe({"farine": 200, "sucre": 100},2,4) == {"farine": 400, "sucre": 200}
+
+def test_desired_nb_per_equal_to_zero() :
+    assert print_desirable_recipe({"farine": 200, "sucre": 100},2,0) == {"farine": 0, "sucre": 0}
