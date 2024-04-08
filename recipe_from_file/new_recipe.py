72b4808035fe0b263604e89bcb4dbf379d6923a0
sarrabah <sarrabenarbia1996@gmail.com>
2024-04-08 16:21:13 +0200

    feat: Implement the dependency inversion concept

--- recipe_from_file/new_recipe.py
+++ recipe_from_file/new_recipe.py
@@ -1,36 +1,53 @@
 from recipe import multiply_quantity_for_n_people
+from abc import ABC, abstractmethod
 
-def read_recipe_from_csv_file(file_path):
+class RecipeReaderServiceInterface(ABC):
+    @abstractmethod
+    def read_recipe_from_csv_file(self, file_path):
+        ...
 
-    file= open(file_path)
-    content = file.readlines()
-    rows = content[1:]
+class RecipeReaderService(RecipeReaderServiceInterface):
 
-    recipe=[]
-    for row in range(len(rows)):
-        element = rows[row].strip().split(",")
-        recipe.append(element)
-    file.close()
+    def read_recipe_from_csv_file(self, file_path):
 
-    return recipe 
+        file= open(file_path)
+        content = file.readlines()
+        rows = content[1:]
 
-def modify_the_original_recipe(original_recipe_from_file,original_number_of_person, desired_number_of_person):
+        recipe=[]
+        for row in range(len(rows)):
+            element = rows[row].strip().split(",")
+            recipe.append(element)
+        file.close()
 
-    original_recipe= {}  
-    desired_recipe= {} 
-    for arr in original_recipe_from_file:
-        original_recipe.update({arr[0]: int(arr[1])})  
+        return recipe 
+
+
+class RecipeAdapter :
+
+    def __init__(self, original_recipe_from_file : RecipeReaderServiceInterface):
+        self.original_recipe_from_file = original_recipe_from_file
+
+    def modify_the_original_recipe(self, original_recipe_from_file,original_number_of_person, desired_number_of_person):
+
+        original_recipe_from_file= {}  
+        desired_recipe= {} 
+        for arr in original_recipe_from_file:
+            original_recipe_from_file.update({arr[0]: int(arr[1])})  
     
-    desired_recipe  = multiply_quantity_for_n_people(original_recipe,original_number_of_person, desired_number_of_person )
+        desired_recipe  = multiply_quantity_for_n_people(original_recipe_from_file,original_number_of_person, desired_number_of_person )
     
-    return desired_recipe
+        return desired_recipe
 
-def multiply_quantity_for_n_people_from_file(file_path, original_number_of_person, desired_number_of_person):
+    def multiply_quantity_for_n_people_from_file(self, file_path, original_number_of_person, desired_number_of_person):
    
-    original_recipe_from_file = read_recipe_from_csv_file(file_path)
-    desired_recipe = modify_the_original_recipe(original_recipe_from_file,original_number_of_person, desired_number_of_person)
-    return desired_recipe
+        original_recipe_from_file = self.original_recipe_from_file.read_recipe_from_csv_file(file_path)
+        desired_recipe = self.modify_the_original_recipe(original_recipe_from_file, original_number_of_person, desired_number_of_person)
+       
+        return desired_recipe
 
 
+recipe_reader = RecipeReaderService()
+new_date = RecipeAdapter(recipe_reader)
 if __name__ == "__main__" :
-    print(multiply_quantity_for_n_people_from_file("./recipe_data.csv", 2, 4))
+    print(new_date. multiply_quantity_for_n_people_from_file('./recipe_data.csv', 2, 4))
