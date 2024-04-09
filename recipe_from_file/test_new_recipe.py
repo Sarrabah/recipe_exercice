fc92daf0f13fd3ccbd0bb65d550ab540d9578b4f
sarrabah <sarrabenarbia1996@gmail.com>
2024-04-09 12:09:54 +0200

    test: Add a unit test when the expected is equal to the desired number of people

--- recipe_from_file/test_new_recipe.py
+++ recipe_from_file/test_new_recipe.py
@@ -15,3 +15,8 @@ def test_different_original_and_desired_number_of_people():
       recipe_reader = TestRecipeReaderService()
       new_recipe = RecipeAdapter(recipe_reader)
       assert new_recipe.multiply_quantity_for_n_people_from_file('./recipe_data.csv', 2, 4) == {'Farine':800.0 , 'Sucre': 100.0, 'Chocolat': 400.0} 
+
+def test_equal_expected_and_desired_nb_per():
+    recipe_reader = TestRecipeReaderService()
+    new_recipe = RecipeAdapter(recipe_reader)
+    assert new_recipe.multiply_quantity_for_n_people_from_file('./recipe_data.csv',2,2) == {'Farine':400.0 , 'Sucre': 50.0, 'Chocolat': 200.0} 
